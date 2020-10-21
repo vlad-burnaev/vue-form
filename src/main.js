44c72c7365277fbc730473a08ee3115def114f33
Vlad Burnaev <vlad.burn325@gmail.com>
2020-10-21 16:54:35 +0300

    delete tel-input

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueSimpleAlert from "vue-simple-alert"
-import VueTelInput from 'vue-tel-input'
 
 Vue.use(VueSimpleAlert);
-Vue.use(VueTelInput)
 
 Vue.config.productionTip = false
 
